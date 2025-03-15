66012d1acf4c21de212521e5b2b11df4a2df3448
hoagians <178979147+hoagians@users.noreply.github.com>
2025-03-15 03:04:26 -0300

    Update whitelist and stage dates for improved accuracy

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -31,13 +31,13 @@ export const PRICE2 = 0.02;
 export const PRICE3 = 0.03;
 
 // Dates
-export const endWhitelist = new Date(Date.UTC(2025, 2, 14));
-export const startStage1 = new Date(Date.UTC(2025, 2, 15, 0, 0, 0, 0));
+export const endWhitelist = new Date(Date.UTC(2025, 2, 22));
+export const startStage1 = new Date(Date.UTC(2025, 2, 23, 0, 0, 0, 0));
 export const daysAfter = 15; // number of days after launch
-export const endStage1 = new Date(Date.UTC(2025, 2, 22));
-export const startStage2 = new Date(Date.UTC(2025, 2, 23));
-export const endStage2 = new Date(Date.UTC(2025, 2, 29));
-export const startStage3 = new Date(Date.UTC(2025, 2, 30));
+export const endStage1 = new Date(Date.UTC(2025, 2, 29));
+export const startStage2 = new Date(Date.UTC(2025, 2, 30));
+export const endStage2 = new Date(Date.UTC(2025, 3, 5));
+export const startStage3 = new Date(Date.UTC(2025, 3, 6));
 
 // Social media
 export const DISCORD_URL = "https://discord.com/";
