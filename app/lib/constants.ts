c7276742f448a55b01e230a213a61c76b849a928
hoagians <178979147+hoagians@users.noreply.github.com>
2025-03-21 05:56:44 -0300

    Update whitelist and stage dates for improved scheduling

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -25,13 +25,13 @@ export const PRICE2 = 0.02;
 export const PRICE3 = 0.03;
 
 // Dates
-export const endWhitelist = new Date(Date.UTC(2025, 2, 29));
-export const startStage1 = new Date(Date.UTC(2025, 2, 30, 0, 0, 0, 0));
+export const endWhitelist = new Date(Date.UTC(2025, 3, 12));
+export const startStage1 = new Date(Date.UTC(2025, 3, 13, 0, 0, 0, 0));
 export const daysAfter = 15; // number of days after launch
-export const endStage1 = new Date(Date.UTC(2025, 3, 5));
-export const startStage2 = new Date(Date.UTC(2025, 3, 6));
-export const endStage2 = new Date(Date.UTC(2025, 3, 12));
-export const startStage3 = new Date(Date.UTC(2025, 3, 13));
+export const endStage1 = new Date(Date.UTC(2025, 3, 19));
+export const startStage2 = new Date(Date.UTC(2025, 3, 20));
+export const endStage2 = new Date(Date.UTC(2025, 3, 26));
+export const startStage3 = new Date(Date.UTC(2025, 3, 27));
 
 // Social media
 export const DISCORD_URL = "";
